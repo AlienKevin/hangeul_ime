892ca82d6a7264206e20f26514a6a5632fd415e7
Kevin Li <kevinli020508@gmail.com>
2022-10-29 08:59:17 -0400

    Asynchronously load dictionary JSON to prevent blocking main thread

--- Hangeul/State.swift
+++ Hangeul/State.swift
@@ -24,8 +24,13 @@ class State: NSObject {
     static let inputModeChanged = Notification.Name("State.inputModeChanged")
 
     var inputMode: InputMode = .hangeul
-    var krDict: Dictionary = loadJson(filename: "KrDict.json")!
+    var krDict: Dictionary = Dictionary()
     var server = IMKServer(name: Bundle.main.infoDictionary?["InputMethodConnectionName"] as? String, bundleIdentifier: Bundle.main.bundleIdentifier)
+    
+    override init() {
+        super.init()
+        loadJson(filename: "KrDict.json")
+    }
 
     func toggleInputMode(_ nextInputMode: InputMode? = nil) {
         if nextInputMode != nil, self.inputMode == nextInputMode {
@@ -58,19 +63,23 @@ class State: NSObject {
             return ([], false)
         }
     }
+    
+    // Asynchronously load dictionary JSON to prevent blocking the main thread
+    private func loadJson(filename fileName: String) {
+        Task.init(priority: TaskPriority.medium) {
+            guard let asset = NSDataAsset(name: fileName) else {
+                fatalError("Missing data asset: \(fileName)")
+            }
+            let decoder = JSONDecoder()
+            let rawDict = try! decoder.decode(RawDictionary.self, from: asset.data)
+            var dict = Dictionary();
+            for rawEntries in rawDict {
+                dict[rawEntries.word] = rawEntries.entries
+            }
+            dlog("KrDict loaded!")
+            self.krDict = dict
+        }
+    }
 
     static let shared = State()
-}
-
-private func loadJson(filename fileName: String) -> Dictionary? {
-    guard let asset = NSDataAsset(name: fileName) else {
-        fatalError("Missing data asset: \(fileName)")
-    }
-    let decoder = JSONDecoder()
-    let rawDict = try! decoder.decode(RawDictionary.self, from: asset.data)
-    var dict = Dictionary();
-    for rawEntries in rawDict {
-        dict[rawEntries.word] = rawEntries.entries
-    }
-    return dict
 }
